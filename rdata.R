40aa0b458bc4374b5369986e93e26c6c7cece4f8
Nobody <Nobody@nowhere.to.go>
2015-12-27 19:14:36 -0500

    Final Version of the document

--- rdata.R
+++ rdata.R
@@ -4,27 +4,13 @@ print( system.time( load("sd.rda") ) )
 
 str(sd)
 
-d <- setorder( sd[, .N, by=sd$EVTYPE], -N )
+d <- setorder( sd[, .N, by = EVTYPE], -N )
 str(d)
 print(d)
 
-sd[, EVTYPE := ifelse( toupper(EVTYPE) %in% c("TSTM WIND",
-                                      " TSTM WIND",
-                                      "SEVERE THUNDERSTORM WINDS",
-                                      "THUNDERSTORM WIND",
-                                      "THUNDERSTORM WINDS"), "TDSTM WINDS", EVTYPE )]
+#qq <- d[EVTYPE %like% "HEAVY RAIN"]
+#View(qq)
 
-sd[, EVTYPE := ifelse( toupper(EVTYPE) %in% c("FLOODING",
-                                              "FLOOD",
-                                              "MAJOR FLOOD"), "FLOOD", EVTYPE )]
+r <- setorder( setnames( sd[ , sum(PROPDMG), by=EVTYPE], "V1", "TOTALDMG" ), -TOTALDMG)
+View(r)
 
-sd[, EVTYPE := ifelse( toupper(EVTYPE) %in% c("HURRICANE/TYPHOON",
-                                              "HURRICANE EMILY",
-                                              "HURRICANE ERIN",
-                                              "HURRICANE OPAL",
-                                              "HURRICANE OPAL/HIGH WINDS",
-                                              "TYPHOON"), "HURRICANE", EVTYPE )]
-
-d <- setorder( sd[, .N, by=sd$EVTYPE], -N )
-str(d)
-print(d)
